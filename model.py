842c9f4f81fb7f5bc0eb82cdf7c04b06eecea172
Navya Gupta <160533984+navyagupta0709@users.noreply.github.com>
2026-04-01 10:53:23 -0700

    Update and rename model.pkl to model.py

--- model.py
+++ model.py
@@ -2,15 +2,15 @@ from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import pickle
 
-# dummy data
-X = np.random.rand(100, 5)
-y = np.random.randint(0, 2, 100)
+# dummy data (5 features same as app)
+X = np.random.rand(200, 5)
+y = np.random.randint(0, 2, 200)
 
 model = RandomForestClassifier()
 model.fit(X, y)
 
-# IMPORTANT: binary mode me save
+# IMPORTANT: binary mode
 with open("model.pkl", "wb") as f:
     pickle.dump(model, f)
 
-print("✅ Fresh model.pkl created")
+print("✅ NEW model.pkl created successfully")
